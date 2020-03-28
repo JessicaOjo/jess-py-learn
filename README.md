8fc6324db027fb82893b4d407d3b74451fcd2edd
Jessica <jessicaojo19@gmail.com>
2020-03-29 00:55:54 +0100

    newest changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jess-py-learn
+made a different change 
 this is an example project to understand th use of git on ubuntu
--- jess.py
+++ jess.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def main(msg):
     print(msg)
+    // another comment
     // Adding a different comment to this file.
     // print a message
 
